e99b187873712c30ddeae0ecd952f85f8c29aa4f
huangziyi01 <huangziyi01@baidu.com>
2016-03-24 12:20:42 +0800

    quick push

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,8 +3,11 @@ import {shallow, mount} from 'enzyme'
 import Progress from 'fit-progress'
 
 describe('fit-progress', ()=> {
-    it('内容成功渲染', ()=> {
-        const node = mount(<Progress/>)
-        expect(node.text()).to.contain('进度条插件更新,依赖应该自动更新')
+    it('进度条label显示正常', ()=> {
+        const node = mount(
+            <Progress percent="30"
+                      showLabel={true}/>
+        )
+        expect(node.text()).to.contain('30%')
     })
 })
